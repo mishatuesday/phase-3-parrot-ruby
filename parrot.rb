36bf1402c33e7c403e225db935e457faa2341cd2
Misha Tuesday <mishatuesday@gmail.com>
2022-10-31 11:35:39 -0400

    commit tree c000bbaa9ec14fdac818743692083c44d8cf7c4e

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot (input = 0)
+    if input == 0
+        puts "Squawk!"
+        return "Squawk!"
+    else
+        puts input
+        return input
+    end
+end
